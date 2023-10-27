dbeb3b2a50c2eeaa1677b03209c5e3219a088b1f
swarna1101 <swarnabhasinha@gmail.com>
2023-10-27 13:27:46 +0530

    Add: AMM and Liquidity

--- AMM tests/scripts/lnterract-liquidity.js
+++ AMM tests/scripts/lnterract-liquidity.js
@@ -3,9 +3,9 @@ const { ethers } = require("hardhat");
 async function main() {
   const [deployer] = await ethers.getSigners();
 
-  const UniswapV2Router = "ADDRESS_OF_UNISWAP_V2_ROUTER_CONTRACT";
-  const tokenA = "ADDRESS_OF_TOKEN_A";
-  const tokenB = "ADDRESS_OF_TOKEN_B";
+  const UniswapV2Router = "0x468E5c1A2442CF52aaD5d077365341EAE56749EF";
+  const tokenA = "0x5a4349f4C10B83F6e9239D9682811AEBb2618C35";
+  const tokenB = "0x16C0F1034E9085392FAFc8d03c0a393eB3782233";
   const amountADesired = ethers.utils.parseUnits("100", 18); // Amount of Token A to add
   const amountBDesired = ethers.utils.parseUnits("200", 18); // Amount of Token B to add
   const amountAMin = ethers.utils.parseUnits("90", 18); // Minimum accepted amount of Token A
